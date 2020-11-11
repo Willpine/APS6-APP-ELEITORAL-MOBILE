3fb3b8bf27ccf0bbfb2c65baa6e40038645bd6a6
willpine <willianyuti@gmail.com>
2020-11-11 02:19:31 -0300

    Câmera funcionando e Bibliotecas também

---
+++ app/src/main/java/com/willpine/aps6/MainActivity.java
@@ -0,0 +1,150 @@
+package com.willpine.aps6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.face.FaceRecognizer;
+import org.opencv.face.LBPHFaceRecognizer;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.osgi.OpenCVNativeLoader;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    static {
+        //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        if (!OpenCVLoader.initDebug())
+            Log.d("ERROR", "Unable to load OpenCV");
+        else
+            Log.d("SUCCESS", "OpenCV loaded");
+    }
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+    FaceRecognizer recognizer;
+    private int w, h;
+    CascadeClassifier cascade;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    //Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+
+                    try {
+                        initializeOpenCVDependencies();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 1);
+        //Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+        w = width;
+        h = height;
+
+    }
+
+    public void onCameraViewStopped() {
+    }
+
+    // É aqui que chamamos/retornamos o que está na camera
+    // Método recognize
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        recognize(inputFrame.rgba());
+        return inputFrame.gray();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d("OpenCV", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
+        } else {
+            Log.d("OpenCV", "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
+    }
+
+
+
+    private void initializeOpenCVDependencies() throws IOException {
+        mOpenCvCameraView.enableView();
+        recognizer = LBPHFaceRecognizer.create();
+        System.out.println(recognizer.toString()+ " DEUUUUUUUUUUUUU CEEEEEEEEEEEEEEERTO");
+        cascade = new CascadeClassifier("haarcascade_frontalface_alt2.xml"); //Apontar para arquivo
+        //detector = FeatureDetector.create(FeatureDetector.ORB);
+        //descriptor = DescriptorExtractor.create(DescriptorExtractor.ORB);
+        //matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE_HAMMING);
+        //img1 = new Mat();
+        //AssetManager assetManager = getAssets();
+        //InputStream istr = assetManager.open("a.jpeg");
+        //Bitmap bitmap = BitmapFactory.decodeStream(istr);
+        //Utils.bitmapToMat(bitmap, img1);
+        //Imgproc.cvtColor(img1, img1, Imgproc.COLOR_RGB2GRAY);
+        //img1.convertTo(img1, 0); //converting the image to match with the type of the cameras image
+        //descriptors1 = new Mat();
+        //keypoints1 = new MatOfKeyPoint();
+        //detector.detect(img1, keypoints1);
+        //descriptor.compute(img1, keypoints1, descriptors1);
+
+    }
+
+    public Mat recognize(Mat aInputFrame) {
+        Mat outputImg = new Mat();
+        recognizer = LBPHFaceRecognizer.create();
+        return outputImg;
+    }
+}
